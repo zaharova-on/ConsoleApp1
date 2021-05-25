7faa20782db9ace0dc90ecd31fd3d8d7383e10e9
Ольга Захарова <pearlolga191@rambler.ru>
2021-05-25 13:32:02 +0300

    Решение квадратного уравнения

--- Program.cs
+++ Program.cs
@@ -10,6 +10,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine($"Введите коэф кв уравнения");
+            Console.Write("a=");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b=");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("c=");
+            int c = int.Parse(Console.ReadLine());
+            double d = b * b - 4 * a * c;
+            Console.WriteLine($"Дискриминант = {d}  ");
+
+            double x1 = (-b - Math.Sqrt(d)) / (2 * a);
+            double x2 = (-b + Math.Sqrt(d)) / (2 * a);
+
+            Console.WriteLine($"Корни уравнения x1 = {x1}, x2 = {x2}");
+            Console.ReadKey();
         }
     }
 }
